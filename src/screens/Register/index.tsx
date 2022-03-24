1e575013c2f990df21cd6a610f9c0edadbe4c59b
Robinson Junior <robinson.juniorr@gmail.com>
2022-03-24 00:46:10 -0300

    criando repo no github

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+import React from "react";
+import { Container } from "./styles";
+
+export function Register() {
+  return (
+    <Container>
+      <Text>Register</Text>
+    </Container>
+  );
+}
