0513a8a297950f0c9a49c0a6276bee09ce96974a
Robinson Junior <robinson.juniorr@gmail.com>
2022-03-24 14:58:59 -0300

    componente de select

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -10,6 +10,7 @@ import {
 import { Input } from "../../components/Form/Input";
 import { Button } from "../../components/Form/Button";
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
+import { CategorySelect } from "../../components/Form/CategorySelect";
 
 export function Register() {
   const [transactionType, setTransactionType] = useState("");
@@ -41,6 +42,7 @@ export function Register() {
               isActive={transactionType === "down"}
             />
           </TransactionsTypes>
+          <CategorySelect title="Categoria" />
         </Fields>
         <Button title="Enviar" />
       </Form>
