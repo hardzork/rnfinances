d184df3f2d94228cf551ac5c24817057790d394c
Robinson Junior <robinson.juniorr@gmail.com>
2022-03-30 12:10:11 -0300

    utilizando a bottom tab navigator

--- App.tsx
+++ App.tsx
@@ -11,9 +11,9 @@ import AppLoading from "expo-app-loading";
 
 import theme from "./src/global/styles/theme";
 
-import { Dashboard } from "./src/screens/Dashboard";
-import { Register } from "./src/screens/Register";
-import { CategorySelect } from "./src/screens/CategorySelect";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { AppRoutes } from "./src/routes/app.routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,9 +26,9 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      {/* <CategorySelect /> */}
-      <Register />
-      {/* <Dashboard /> */}
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
